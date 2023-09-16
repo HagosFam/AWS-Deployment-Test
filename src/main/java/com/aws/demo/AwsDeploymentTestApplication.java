5a561fb84be5cf6ac0a3a13e2d226bbfc4660077
hagos <111482114+Kytsync@users.noreply.github.com>
2023-09-16 11:02:25 -0500

    initial proejct setup

---
+++ src/main/java/com/aws/demo/AwsDeploymentTestApplication.java
@@ -0,0 +1,13 @@
+package com.aws.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDeploymentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDeploymentTestApplication.class, args);
+	}
+
+}
